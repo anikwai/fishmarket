1c7d3fe071710c0c011076000b5bb86bb391233b
anikwai <anikwai.w@gmail.com>
2025-12-03 18:42:06 +1100

    feat: add ID column with sorting functionality to Sales index page

--- resources/js/pages/Sales/Index.tsx
+++ resources/js/pages/Sales/Index.tsx
@@ -656,6 +656,30 @@ export default function SalesIndex({
                 enableHiding: false,
             },
             {
+                accessorKey: 'id',
+                header: ({ column }) => {
+                    return (
+                        <Button
+                            variant="ghost"
+                            onClick={() =>
+                                column.toggleSorting(
+                                    column.getIsSorted() === 'asc',
+                                )
+                            }
+                            className="h-8 px-2 lg:px-3"
+                        >
+                            ID
+                            <ArrowUpDown className="ml-2 h-4 w-4" />
+                        </Button>
+                    );
+                },
+                cell: ({ row }) => (
+                    <div className="font-mono text-sm">
+                        {row.getValue('id')}
+                    </div>
+                ),
+            },
+            {
                 accessorKey: 'sale_date',
                 header: ({ column }) => {
                     return (
